e5845108e9c21a3778cab1d83aed538237442422
kikimetal <reezafratzitz2@gmail.com>
2017-11-25 17:03:44 +0900

    gulpfile のいらないコード削除。

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,15 +2,11 @@
 const deploymentDir = "__dist"
 // html file
 const indexHtml = "index.html"
-const discoverHtml = "index.html"
 // entry point js file name
 const indexJs = "index.js"
-const discoverJs = "discover.js"
 // bundle js file name
 const bundleIndexJs = "bundle.js"
 const bundleIndexMinJs = "bundle.js"
-const bundleDiscoverJs = "bundle.js"
-const bundleDiscoverMinJs = "bundle.js"
 // bundle css file name
 const bundleCSS = "bundle.css"
 
@@ -83,8 +79,8 @@ gulp.task("css-min",
         ])
         .pipe(concat(bundleCSS))
         .pipe(cleanCSS({debug: true}, function(details) {
-            console.log(details.name + '(originalSize): ' + details.stats.originalSize);
-            console.log(details.name + '(minifiedSize): ' + details.stats.minifiedSize);
+            console.log(details.name + '(originalSize): ' + details.stats.originalSize)
+            console.log(details.name + '(minifiedSize): ' + details.stats.minifiedSize)
         }))
         .pipe(gulp.dest("./css/"))
     }
@@ -99,10 +95,10 @@ gulp.task("browserify-index", ()=>{
             presets: ["latest", "react"],
         })
         .bundle()
-        .on("error", function (err) { console.log("Error : " + err.message); })
+        .on("error", function (err) { console.log("Error : " + err.message) })
         .pipe(source(bundleIndexJs)) // 出力ファイル名を指定
-        .pipe(gulp.dest("./js/"));
-});
+        .pipe(gulp.dest("./js/"))
+})
 
 gulp.task("browserify-index-min", ()=>{
     return browserify({
@@ -115,46 +111,18 @@ gulp.task("browserify-index-min", ()=>{
         .pipe(source(bundleIndexMinJs)) // 出力ファイル名を指定
         .pipe(buffer())
         .pipe(uglify())
-        .pipe(gulp.dest("./js/"));
-});
-
-gulp.task("browserify-discover", ()=>{
-    return browserify({
-        entries: [`./js/src/${discoverJs}`],
-        debug: true,
-    })
-        .transform(babelify, {
-            presets: ["latest", "react"],
-        })
-        .bundle()
-        .on("error", function (err) { console.log("Error : " + err.message); })
-        .pipe(source(bundleDiscoverJs)) // 出力ファイル名を指定
-        .pipe(gulp.dest("./js/"));
-});
-
-gulp.task("browserify-discover-min", ()=>{
-    return browserify({
-        entries: [`./js/src/${discoverJs}`],
-    })
-        .transform(babelify, {
-            presets: ["latest", "react"],
-        })
-        .bundle()
-        .pipe(source(bundleDiscoverMinJs))
-        .pipe(buffer())
-        .pipe(uglify())
-        .pipe(gulp.dest("./js/"));
-});
+        .pipe(gulp.dest("./js/"))
+})
 
 gulp.task("apply-prod-environment", ()=>{
-    return process.env.NODE_ENV = "production";
-});
+    return process.env.NODE_ENV = "production"
+})
 
 gulp.task("clean", (callback)=>{
     return del([
         `./${deploymentDir}/**/*`,
-    ], callback);
-});
+    ], callback)
+})
 
 gulp.task("dist", // deployment index
     [
@@ -164,29 +132,13 @@ gulp.task("dist", // deployment index
         "browserify-index-min",
     ], ()=>{
         gulp.src(`./${indexHtml}`)
-            .pipe(gulp.dest(`./${deploymentDir}/`));
+            .pipe(gulp.dest(`./${deploymentDir}/`))
         gulp.src(`./image/**/*`)
-            .pipe(gulp.dest(`./${deploymentDir}/image/`));
+            .pipe(gulp.dest(`./${deploymentDir}/image/`))
         gulp.src(`./js/${bundleIndexMinJs}`)
-            .pipe(gulp.dest(`./${deploymentDir}/js/`));
+            .pipe(gulp.dest(`./${deploymentDir}/js/`))
         gulp.src(`./css/${bundleCSS}`)
-            .pipe(gulp.dest(`./${deploymentDir}/css/`));
-    }
-)
-
-gulp.task("dist-discover", // deployment discover
-    [
-        "clean",
-        "apply-prod-environment",
-        "css-min",
-        "browserify-discover-min",
-    ], ()=>{
-        gulp.src(`./${discoverHtml}`)
-            .pipe(gulp.dest(`./${deploymentDir}/`));
-        gulp.src(`./js/${bundleDiscoverMinJs}`)
-            .pipe(gulp.dest(`./${deploymentDir}/js/`));
-        gulp.src(`./css/${bundleCSS}`)
-            .pipe(gulp.dest(`./${deploymentDir}/css/`));
+            .pipe(gulp.dest(`./${deploymentDir}/css/`))
     }
 )
 
@@ -198,20 +150,6 @@ gulp.task("watch", ["default"], ()=>{
         "browserify-index",
     ])
 })
-
-gulp.task("watch-discover", ["discover"], ()=>{
-    gulp.watch("./css/**/*", [
-        "css",
-    ])
-    gulp.watch("./js/src/**/*.js", [
-        "browserify-discover",
-    ])
-})
-
-gulp.task("discover", [
-    "css",
-    "browserify-discover",
-])
 
 gulp.task("default", [
     "css",
